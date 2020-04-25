3e229cea7be97613a39c4266832b24b100258641
Dominic Griesel <d.griesel@gmx.net>
2020-04-25 10:57:28 +0200

    build: remove build folder from Github

--- build/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { DB } from "./lib/db";
-//# sourceMappingURL=index.d.ts.map
--- build/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var db_1 = require("./lib/db");
-exports.DB = db_1.DB;
-//# sourceMappingURL=index.js.map
