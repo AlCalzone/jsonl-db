931cb2b3fefcc747d98f4a313987c9e4a7be3c0e
Dominic Griesel <d.griesel@gmx.net>
2021-12-27 14:48:57 +0100

    feat: stringify entries lazily, deduplicate chunks on write

--- test/perf.ts
+++ test/perf.ts
@@ -1,42 +1,72 @@
+import { padStart } from "alcalzone-shared/strings";
+import fs from "fs-extra";
 import { JsonlDB } from "../src";
+
+process.on("unhandledRejection", (r) => {
+	debugger;
+});
 
 const testDB: JsonlDB<any> = new JsonlDB("test.jsonl", {
 	autoCompress: { onClose: false },
 	throttleFS: {
-		intervalMs: 10000,
+		intervalMs: 1000,
 	},
 });
 
 (async () => {
-	// await testDB.open();
-	// // add a shitton of values
-	// console.time("create values");
-	// const MAX_NODES = 100;
-	// for (let pass = 1; pass <= 10; pass++) {
-	// 	for (let nodeId = 1; nodeId <= MAX_NODES; nodeId++) {
-	// 		for (let ccId = 1; ccId <= 100; ccId++) {
-	// 			for (let endpoint = 0; endpoint <= 10; endpoint++) {
-	// 				for (const property of ["a", "b", "c", "d", "e"]) {
-	// 					const key = `${nodeId}-${ccId}-${endpoint}-${property}`;
-	// 					if (Math.random() < 0.15) {
-	// 						testDB.delete(key);
-	// 					} else {
-	// 						testDB.set(key, Math.random() * 100);
-	// 					}
-	// 				}
-	// 			}
-	// 		}
-	// 	}
-	// }
+	// add a shitton of values
+	const NUM_PASSES = 10;
+	const NUM_KEYS = 1000;
+	const NUM_CHANGES = 100000;
+	let total: number = 0;
+
+	// console.time("open");
+	// console.timeEnd("open");
+
+	for (let pass = 1; pass <= NUM_PASSES; pass++) {
+		await fs.remove("test.jsonl");
+
+		await testDB.open();
+
+		console.log(`start ${pass}`);
+
+		const start = Date.now();
+		for (let i = 0; i < NUM_CHANGES; i++) {
+			const key = `k${padStart(
+				Math.round(Math.random() * NUM_KEYS).toString(),
+				5,
+				"0",
+			)}`;
+			if (Math.random() < 0.15) {
+				testDB.delete(key);
+			} else {
+				testDB.set(key, Math.random() * 100);
+			}
+		}
+		console.log("close");
+		await testDB.close();
+
+		const time = Date.now() - start;
+		total += time;
+
+		console.log(`end ${pass}`);
+	}
+	// console.time("close");
 	// await testDB.close();
-	// console.timeEnd("create values");
+	// 	console.timeEnd("close");
 
-	console.time("open values");
-	await testDB.open();
-	console.log(testDB.size);
-	console.timeEnd("open values");
+	console.log(`${NUM_PASSES}x, ${NUM_KEYS} keys, ${NUM_CHANGES} changes`);
+	console.log(`  ${(total / NUM_PASSES).toFixed(2)} ms / attempt`);
+	console.log(`  ${((NUM_CHANGES / total) * 1000).toFixed(2)} changes/s`);
 
-	await testDB.close();
+	process.exit(0);
+
+	// console.time("open values");
+	// await testDB.open();
+	// console.log(testDB.size);
+	// console.timeEnd("open values");
+
+	// await testDB.close();
 
 	// await fs.remove("test.jsonl");
 })().catch(() => {
