e987315da2c3aa5c0673c8f2142e5cc547310498
Dominic Griesel <d.griesel@gmx.net>
2021-12-30 10:37:39 +0100

    test: keep both tests

--- test/perf.ts
+++ test/perf.ts
@@ -6,29 +6,90 @@ process.on("unhandledRejection", (r) => {
 	debugger;
 });
 
-const testDB: JsonlDB<any> = new JsonlDB("test.jsonl", {
-	autoCompress: { onClose: false },
-	throttleFS: {
-		intervalMs: 1000,
-	},
-});
+async function testMedium() {
+	const testDB: JsonlDB<any> = new JsonlDB("test.jsonl", {
+		autoCompress: {
+			sizeFactor: 2,
+			sizeFactorMinimumSize: 5000,
+		},
+		ignoreReadErrors: true,
+		throttleFS: {
+			intervalMs: 60000,
+			maxBufferedCommands: 100,
+		},
+	});
 
-(async () => {
-	// add a shitton of values
+	function makeObj(i: number) {
+		return {
+			type: "state",
+			common: {
+				name: i.toString(),
+				read: true,
+				write: true,
+				role: "state",
+				type: "number",
+			},
+			native: {},
+		};
+	}
+
 	const NUM_PASSES = 10;
-	const NUM_KEYS = 1000;
-	const NUM_CHANGES = 100000;
+	const NUM_OBJECTS = 100000;
 	let total: number = 0;
 
-	// console.time("open");
-	// console.timeEnd("open");
+	console.log("start test MEDIUM");
 
 	for (let pass = 1; pass <= NUM_PASSES; pass++) {
 		await fs.remove("test.jsonl");
 
 		await testDB.open();
 
-		console.log(`start ${pass}`);
+		const start = Date.now();
+		for (let i = 0; i < NUM_OBJECTS; i++) {
+			const key = `benchmark.0.test${i}`;
+			const value = makeObj(i);
+			testDB.set(key, value);
+		}
+
+		await testDB.close();
+
+		const time = Date.now() - start;
+		total += time;
+
+		process.stdout.write(".");
+	}
+
+	await fs.remove("test.jsonl");
+
+	process.stdout.write("\n\n");
+
+	console.log(`${NUM_PASSES}x, ${NUM_OBJECTS} objects`);
+	console.log(`  ${(total / NUM_PASSES).toFixed(2)} ms / attempt`);
+	console.log(`  ${((NUM_OBJECTS / total) * 1000).toFixed(2)} changes/s`);
+	console.log();
+	console.log();
+}
+
+async function testSmall() {
+	const testDB: JsonlDB<any> = new JsonlDB("test.jsonl", {
+		autoCompress: { onClose: false },
+		throttleFS: {
+			intervalMs: 1000,
+		},
+	});
+
+	// add a shitton of values
+	const NUM_PASSES = 10;
+	const NUM_KEYS = 1000;
+	const NUM_CHANGES = 100000;
+	let total: number = 0;
+
+	console.log("start test SMALL");
+
+	for (let pass = 1; pass <= NUM_PASSES; pass++) {
+		await fs.remove("test.jsonl");
+
+		await testDB.open();
 
 		const start = Date.now();
 		for (let i = 0; i < NUM_CHANGES; i++) {
@@ -43,32 +104,29 @@ const testDB: JsonlDB<any> = new JsonlDB("test.jsonl", {
 				testDB.set(key, Math.random() * 100);
 			}
 		}
-		console.log("close");
 		await testDB.close();
 
 		const time = Date.now() - start;
 		total += time;
 
-		console.log(`end ${pass}`);
+		process.stdout.write(".");
 	}
-	// console.time("close");
-	// await testDB.close();
-	// 	console.timeEnd("close");
+
+	await fs.remove("test.jsonl");
+
+	process.stdout.write("\n\n");
 
 	console.log(`${NUM_PASSES}x, ${NUM_KEYS} keys, ${NUM_CHANGES} changes`);
 	console.log(`  ${(total / NUM_PASSES).toFixed(2)} ms / attempt`);
 	console.log(`  ${((NUM_CHANGES / total) * 1000).toFixed(2)} changes/s`);
+	console.log();
+	console.log();
+}
 
-	process.exit(0);
-
-	// console.time("open values");
-	// await testDB.open();
-	// console.log(testDB.size);
-	// console.timeEnd("open values");
-
-	// await testDB.close();
-
-	// await fs.remove("test.jsonl");
-})().catch(() => {
-	/* ignore */
-});
+testSmall()
+	.then(testMedium)
+	.catch(console.error)
+	.finally(() => fs.remove("test.jsonl"))
+	.catch(() => {
+		/* ignore */
+	});
